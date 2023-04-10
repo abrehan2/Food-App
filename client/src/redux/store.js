eedf726cb136ddec930ed166df18beed72c4f66a
Abdul Rehan <100872683+abrehan2@users.noreply.github.com>
2023-04-10 17:01:34 +0500

    initial commit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -16,5 +16,5 @@ const store = configureStore({
   },
 });
 
-export const server = `http://localhost:4000/foodApp`;
+export const server = `https://food-app-git-main-abrehan2.vercel.app/foodApp`;
 export default store;
